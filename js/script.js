491a3acefcad699887af2a4f3cd568d346c94cae
faeze <faeze_zakipour@yahoo.com>
2023-12-02 14:10:02 -0800

    v2

--- js/script.js
+++ js/script.js
@@ -12,10 +12,31 @@ close1.addEventListener("click",function(){
 
 
 // robat
-let box1 =document.getElementById("box1");
-let input =document.getElementById("btn");
- let box =document.getElementById("box");
-input.addEventListener("click" , function(){
-  box1.style.display="none"
-  box.style.display="block"
-})
+let robat = document.getElementById("robat");
+let check = document.getElementById("check");
+let pedar = document.getElementById("pedar");
+let text = document.getElementById("text");
+let random = document.getElementById("random");
+let change = document.getElementById("change");
+let sabt = document.getElementById("sabt");
+let tik = document.getElementById("tik");
+let num1;
+let num2;
+check.addEventListener("click", function () {
+  robat.style.display = "none";
+  pedar.style.display = "block";
+});
+random.innerHTML = `${Math.floor(Math.random() * 100000)}`;
+change.addEventListener("click", function () {
+  random.innerHTML = `${Math.floor(Math.random() * 100000)}`;
+});
+sabt.addEventListener("click", function () {
+  if (text.value === random.innerHTML) {
+   robat.style.display = "none";
+   pedar.style.display = "none";
+   tik.style.display = "block"
+  } else alert("لطفا متن ‌صحیح را وارد نمایید :))) ");
+  text.value = "";
+  random.innerHTML = `${Math.floor(Math.random() * 10000000)}`;
+
+});
